2fed67fb7043fbb308a005ec0e95590ee0f6c6e7
BluSnake <minewhip@gmail.com>
2024-11-04 12:55:23 -0700

    change vscode from flatpak to dnf

--- FullInstall.sh
+++ FullInstall.sh
@@ -101,7 +101,10 @@ sudo dnf install flatseal $args
 
 #Installing VS Code
 if [ $vscode = 'Y' ]; then
-	sudo flatpak install com.visualstudio.code $args
+	sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+	echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+	dnf check-update
+	sudo dnf install code # or code-insiders
 fi
 
 #Installing Wireshark
